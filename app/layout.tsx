ac728031999ee2c0626f2f2bd973b87b291c95b6
Evgin Ehad <ehad.evgin@uzh.ch>
2026-03-28 19:03:27 +0100

    moved AntdRegistry to outermost

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,46 +44,45 @@ export default function RootLayout({
 
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ConfigProvider
-          theme={{
-            algorithm: theme.defaultAlgorithm,
-            token: {
-              // general theme options are set in token, meaning all primary elements (button, menu, ...) will have this color
-              colorPrimary: "#22426b", // selected input field boarder will have this color as well
-              borderRadius: 8,
-              colorText: "#fff",
-              fontSize: 16,
+        <AntdRegistry>
+          <ConfigProvider
+            theme={{
+              algorithm: theme.defaultAlgorithm,
+              token: {
+                // general theme options are set in token, meaning all primary elements (button, menu, ...) will have this color
+                colorPrimary: "#22426b", // selected input field boarder will have this color as well
+                borderRadius: 8,
+                colorText: "#fff",
+                fontSize: 16,
 
-              // Alias Token
-              colorBgContainer: "#16181D",
-            },
-            // if a component type needs special styling, setting here will override default options set in token
-            components: {
-              Button: {
-                colorPrimary: "#75bd9d", // this will color all buttons in #75bd9d, overriding the default primaryColor #22426b set in token line 35
-                algorithm: true, // enable algorithm (redundant with line 33 but here for demo purposes)
-                controlHeight: 38,
+                // Alias Token
+                colorBgContainer: "#16181D",
               },
-              Input: {
-                colorBorder: "gray", // color boarder selected is not overridden but instead is set by primary color in line 35
-                colorTextPlaceholder: "#888888",
-                algorithm: false, // disable algorithm (line 32)
+              // if a component type needs special styling, setting here will override default options set in token
+              components: {
+                Button: {
+                  colorPrimary: "#75bd9d", // this will color all buttons in #75bd9d, overriding the default primaryColor #22426b set in token line 35
+                  algorithm: true, // enable algorithm (redundant with line 33 but here for demo purposes)
+                  controlHeight: 38,
+                },
+                Input: {
+                  colorBorder: "gray", // color boarder selected is not overridden but instead is set by primary color in line 35
+                  colorTextPlaceholder: "#888888",
+                  algorithm: false, // disable algorithm (line 32)
+                },
+                Form: {
+                  labelColor: "#fff",
+                  algorithm: theme.defaultAlgorithm, // specify a specifc algorithm instead of true/false
+                },
+                Card: {},
               },
-              Form: {
-                labelColor: "#fff",
-                algorithm: theme.defaultAlgorithm, // specify a specifc algorithm instead of true/false
-              },
-              Card: {},
-            },
-          }}
-        >
-          <MuiProvider>
-            <AntdRegistry>
+            }}
+          >
+            <MuiProvider>
               <AntdApp>{children}</AntdApp>
-            </AntdRegistry>
-          </MuiProvider>
-        </ConfigProvider>
-
+            </MuiProvider>
+          </ConfigProvider>
+        </AntdRegistry>
       </body>
     </html>
   );
